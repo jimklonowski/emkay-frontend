da21e0c5ed1877d5ee05c003ec42bd28ff8fff76
Jim Klonowski <jklonowski@emkay.com>
2020-04-09 09:45:43 -0500

    excel downloader component

---
+++ components/core/ExcelDownloader.vue
@@ -0,0 +1,55 @@
+<template>
+  <client-only>
+    <download-excel :fields="downloadFields" :data="items" :name="exportName">
+      <v-btn
+        :title="`${$t('save')} .xls`"
+        :color="color"
+        :large="large"
+        :icon="icon && !text"
+        :outlined="outlined"
+      >
+        <v-icon v-if="icon" v-text="icon" />
+        <span v-if="text" class="ml-2">{{ text }}</span>
+      </v-btn>
+    </download-excel>
+  </client-only>
+</template>
+
+<script>
+export default {
+  props: {
+    color: {
+      type: String,
+      default: 'primary'
+    },
+    downloadFields: {
+      type: Object,
+      required: true
+    },
+    exportName: {
+      type: String,
+      required: true
+    },
+    icon: {
+      type: [String, Boolean],
+      default: 'mdi-cloud-download'
+    },
+    items: {
+      type: Array,
+      required: true
+    },
+    large: {
+      type: Boolean,
+      default: true
+    },
+    outlined: {
+      type: Boolean,
+      default: false
+    },
+    text: {
+      type: String,
+      default: null
+    }
+  }
+}
+</script>
