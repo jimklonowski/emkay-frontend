83047f0131a61e16f9eea73a52780edefaaf093a
Jim Klonowski <jklonowski@emkay.com>
2020-04-09 07:51:32 -0500

    account middleware

--- middleware/account.js
+++ middleware/account.js
@@ -1,3 +1,7 @@
+/**
+ * This middleware runs on account pages.
+ * If account store is not initialized, calls account/init
+ */
 export default async ({ store }) => {
   if (!store.getters['account/isInitialized']) {
     await store.dispatch('account/init')
