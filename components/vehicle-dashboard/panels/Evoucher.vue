be05f998e0495559a752e76198b855495de0f4c6
Jim Klonowski <jklonowski@emkay.com>
2020-04-17 07:36:45 -0500

    evoucher vuex

--- components/vehicle-dashboard/panels/Evoucher.vue
+++ components/vehicle-dashboard/panels/Evoucher.vue
@@ -1,9 +1,35 @@
 <template>
-  <h1>evoucher</h1>
+  <v-card flat tile>
+    <v-card-title>{{ $t('evoucher') }}</v-card-title>
+    <v-card-text>{{ evoucher }}</v-card-text>
+  </v-card>
 </template>
 
 <script>
+import { mapActions, mapGetters } from 'vuex'
 export default {
-  name: 'Evoucher'
+  /**
+   * Fetch eVoucher
+   */
+  async fetch () {
+    await this.fetchEvoucher()
+  },
+  computed: {
+    /**
+     * Vuex Getters
+     */
+    ...mapGetters({
+      evoucher: 'vehicle-dashboard/getEvoucher',
+      vehicle_number: 'vehicle-dashboard/getVehicleNumber'
+    })
+  },
+  methods: {
+    /**
+     * Vuex Actions
+     */
+    ...mapActions({
+      fetchEvoucher: 'vehicle-dashboard/fetchEvoucher'
+    })
+  }
 }
 </script>
